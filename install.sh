c9c793c8b60dd3d395ae1fbf977132f1c2f9f9da
Mardava Gubbi <mardava.gubbi@teamindus.in>
2017-06-20 10:44:35 +0530

    Added soft link for vim airline theme.

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 ln -s plugins/NewProggie-Color-Scheme/colors/newproggie.vim colors/newproggie.vim
 git clone https://github.com/powerline/fonts.git ~/fonts
 cd ~/fonts && ./install.sh && cd ~ && rm -rf fonts
+ln -s plugins/NewProggie-Color-Scheme/vim-airline-themes/newproggie.vim plugins/vim-airline-themes/plugin/newproggie.vim
