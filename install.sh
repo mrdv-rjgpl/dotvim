a09192f25378fc8deaa71276d21fe58828ac616b
Vishal Raveendranathan <vishal.r@teamindus.in>
2017-06-23 13:03:16 +0530

    Fixed symlink issue with newproggie

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 ln -s ~/.vim/plugins/NewProggie-Color-Scheme/colors/newproggie.vim ~/.vim/colors/newproggie.vim
-#git clone https://github.com/powerline/fonts.git ~/fonts
-#cd ~/fonts && ./install.sh && cd ~ && rm -rf fonts
+git clone https://github.com/powerline/fonts.git ~/fonts
+cd ~/fonts && ./install.sh && cd ~ && rm -rf fonts
 ln -s ~/.vim/plugins/NewProggie-Color-Scheme/vim-airline-themes/newproggie.vim ~/.vim/plugins/vim-airline-themes/plugin/newproggie.vim
