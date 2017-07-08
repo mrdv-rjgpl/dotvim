27acf5c15d7a388536a51729f567bc92f7cc75a6
Mardava Gubbi <mardava.gubbi@teamindus.in>
2017-07-08 08:37:17 +0530

    1. Updated install script to create a softlink for the .vimrc file in the home folder. 2. Added print statements to install script. 3. Updated README to include installation instructions.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# dotvim
-My dotvim folder.
+dotvim
+======
+My .vim folder.
+
+Setup as follows:
+1. Clone this repository and set it up as your .vim folder.
+	```
+	cd
+	git clone https://github.com/mrdv-rjgpl/dotvim.git .vim
+	```
+2. Run the installation script.
+	```
+	cd ~/.vim
+	./install.sh
+	```
+
+That's it!
--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 #ln -s ~/.vim/plugins/NewProggie-Color-Scheme/colors/newproggie.vim ~/.vim/colors/newproggie.vim
+echo "Creating symbolic link for file .vimrc..."
+ln -s ~/.vim/vimrc ~/.vimrc
+echo "Installing plugins..."
 vi -c "PlugInstall | q | q"
+echo "Installing powerline fonts..."
 git clone https://github.com/powerline/fonts.git ~/fonts
 cd ~/fonts && ./install.sh && cd ~ && rm -rf fonts
+echo "Updating airline color scheme to newproggie..."
 ln -s ~/.vim/plugins/NewProggie-Color-Scheme/vim-airline-themes/newproggie.vim ~/.vim/plugins/vim-airline-themes/plugin/newproggie.vim
