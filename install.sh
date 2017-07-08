49887b4c15bc1f034fd5eca6fbce3e7baab78df2
Mardava Gubbi <mardava.gubbi@teamindus.in>
2017-07-08 08:26:57 +0530

    Added PlugInstall command to install script.

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #ln -s ~/.vim/plugins/NewProggie-Color-Scheme/colors/newproggie.vim ~/.vim/colors/newproggie.vim
+vi -c "PlugInstall | q | q"
 git clone https://github.com/powerline/fonts.git ~/fonts
 cd ~/fonts && ./install.sh && cd ~ && rm -rf fonts
 ln -s ~/.vim/plugins/NewProggie-Color-Scheme/vim-airline-themes/newproggie.vim ~/.vim/plugins/vim-airline-themes/plugin/newproggie.vim
