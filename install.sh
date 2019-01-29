863cba00b21004fe2915850f13c6af973730a129
Mardava Gubbi <mrdv.rjgpl@gmail.com>
2019-01-29 15:50:11 -0500

    Switch from airline to powerline
    Unresolved issues with airline, decided to skip the hassle and move to powerline.

--- install.sh
+++ install.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
-#ln -s ~/.vim/plugins/NewProggie-Color-Scheme/colors/newproggie.vim ~/.vim/colors/newproggie.vim
 echo "Creating symbolic link for file .vimrc..."
 ln -s ~/.vim/vimrc ~/.vimrc
-echo "Installing plugins..."
-vi -c "PlugInstall | q | q"
-echo "Installing exuberant-ctags"
-sudo apt install exuberant-ctags
+echo "Installing exuberant-ctags and pypy..."
+sudo apt install exuberant-ctags pypy python3-pip
+echo "Installing powerline and psutil..."
+pip3 install powerline-status psutil
 echo "Installing powerline fonts..."
 git clone https://github.com/powerline/fonts.git ~/fonts
 cd ~/fonts && ./install.sh && cd ~ && rm -rf fonts
-echo "Updating airline color scheme to newproggie..."
-ln -s ~/.vim/plugins/NewProggie-Color-Scheme/vim-airline-themes/newproggie.vim ~/.vim/plugins/vim-airline-themes/plugin/newproggie.vim
