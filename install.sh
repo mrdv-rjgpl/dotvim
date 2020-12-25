a4bf60113915838079a1178f124b07cab0751329
Mardava Gubbi <mrdv.rjgpl@gmail.com>
2020-12-25 13:41:22 -0500

    Add newproggie color file and remove from install steps

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-mkdir -p ${HOME}/.vim/colors
+vi -c "PlugInstall"
 mkdir -p ${HOME}/.vim/plugged/vim-airline-themes/autoload/airline/themes/
-wget https://raw.githubusercontent.com/NewProggie/NewProggie-Color-Scheme/master/colors/newproggie.vim -O ${HOME}/.vim/colors/newproggie.vim
 wget https://raw.githubusercontent.com/NewProggie/NewProggie-Color-Scheme/master/vim-airline-themes/newproggie.vim -O ${HOME}/.vim/plugged/vim-airline-themes/autoload/airline/themes/newproggie.vim
